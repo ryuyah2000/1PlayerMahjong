37f86ac9c5a073dfdc64ebfd5fbd639cec2725d5
ryuyah2000 <ryuyah2000@users.noreply.github.com>
2024-10-02 12:27:51 -0700

    implement wall creation, hand creation, hand printing
    changed hand from array<int> to vector<int>

--- main.cpp
+++ main.cpp
@@ -5,86 +5,116 @@
 #include <iostream>
 #include <map>
 #include <array>
+#include <vector>
 #include <random>
 #include <ctime>
 #include <algorithm> // shuffle
 
 
-void initialize_tile_names(std::map<std::string, int> tiles) {
-    // manzu
-    tiles.insert({"1m", 0x11});
-    tiles.insert({"2m", 0x12});
-    tiles.insert({"3m", 0x13});
-    tiles.insert({"4m", 0x14});
-    tiles.insert({"5m", 0x15});
-    tiles.insert({"6m", 0x16});
-    tiles.insert({"7m", 0x17});
-    tiles.insert({"8m", 0x18});
-    tiles.insert({"9m", 0x19});
-    
-    // pinzu
-    tiles.insert({"1p", 0x21});
-    tiles.insert({"2p", 0x22});
-    tiles.insert({"3p", 0x23});
-    tiles.insert({"4p", 0x24});
-    tiles.insert({"5p", 0x25});
-    tiles.insert({"6p", 0x26});
-    tiles.insert({"7p", 0x27});
-    tiles.insert({"8p", 0x28});
-    tiles.insert({"9p", 0x29});
-    
-    // souzu
-    tiles.insert({"1s", 0x31});
-    tiles.insert({"2s", 0x32});
-    tiles.insert({"3s", 0x33});
-    tiles.insert({"4s", 0x34});
-    tiles.insert({"5s", 0x35});
-    tiles.insert({"6s", 0x36});
-    tiles.insert({"7s", 0x37});
-    tiles.insert({"8s", 0x38});
-    tiles.insert({"9s", 0x39});
-    
-    // honor tiles
-    tiles.insert({"E", 0x41});
-    tiles.insert({"S", 0x42});
-    tiles.insert({"W", 0x43});
-    tiles.insert({"N", 0x44});
-    tiles.insert({"W", 0x45});
-    tiles.insert({"G", 0x46});
-    tiles.insert({"R", 0x47});
-}
-
-
-void initialize_wall(std::array<int, 136> &wall, std::map<std::string, int> tiles) {
+// puts 4 copies of each tile into the wall
+void initialize_wall(std::array<int, 136> &wall, const std::map<int, std::string> tiles) {
     int i = 0;
-    for (const std::pair<std::string, int> t : tiles) {
+    for (std::pair<int, std::string> t : tiles) {
         // put 4 copies of each tile into the wall
-        wall.at(i++) = t.second;
-        wall.at(i++) = t.second;
-        wall.at(i++) = t.second;
-        wall.at(i++) = t.second;
+        wall.at(i++) = t.first;
+        wall.at(i++) = t.first;
+        wall.at(i++) = t.first;
+        wall.at(i++) = t.first;
     }
 }
 
 
+// shuffles wall
 void shuffle_wall(std::array<int, 136> &wall) {
     std::mt19937 rng(std::time(nullptr));
     std::shuffle(wall.begin(), wall.end(), rng);
 }
 
 
+// generates hand from the first 14 tiles of the wall
+int generate_hand(std::array<int, 136> wall, std::vector<int> &hand) {
+    for (int i = 0; i < 14; ++i) {
+        hand.at(i) = wall.at(i);
+    }
+    return 14;
+}
+
+
+void print_hand(std::vector<int> &hand, const std::map<int, std::string> tile_names) {
+    for (int i : hand) {
+        std::cout << tile_names.at(i);
+    }
+    std::cout << std::endl;
+}
+
+void sort_hand(std::vector<int> &hand) {
+    std::sort(hand.begin(), hand.end());
+}
+
+
 int main() {
-    std::map<std::string, int> tile_names; // map tile names to integer values for easier hand completeness calculation
+    // map tile names to integer values for easier hand completeness calculation
+    const std::map<int, std::string> tile_names {
+        // manzu
+        { 0x11, "1m" },
+        { 0x12, "2m" },
+        { 0x13, "3m" },
+        { 0x14, "4m" },
+        { 0x15, "5m" },
+        { 0x16, "6m" },
+        { 0x17, "7m" },
+        { 0x18, "8m" },
+        { 0x19, "9m" },
+        
+        // pinzu
+        { 0x21, "1p" },
+        { 0x22, "2p" },
+        { 0x23, "3p" },
+        { 0x24, "4p" },
+        { 0x25, "5p" },
+        { 0x26, "6p" },
+        { 0x27, "7p" },
+        { 0x28, "8p" },
+        { 0x29, "9p" },
+        
+        // souzu
+        { 0x31, "1s" },
+        { 0x32, "2s" },
+        { 0x33, "3s" },
+        { 0x34, "4s" },
+        { 0x35, "5s" },
+        { 0x36, "6s" },
+        { 0x37, "7s" },
+        { 0x38, "8s" },
+        { 0x39, "9s" },
+        
+        // honor tiles
+        { 0x41, "E" },
+        { 0x42, "S" },
+        { 0x43, "W" },
+        { 0x44, "N" },
+        { 0x45, "Wh" },
+        { 0x46, "Gr" },
+        { 0x47, "Re" }
+    };
     
     
     std::array<int, 136> wall; // 136 tiles total. 4 of each type of tile.
-    std::array<int, 14> hand; // 14 tiles in your hand
+    std::vector<int> hand(14); // 14 tiles in your hand
     int depth; // how deep into the wall you are
+    initialize_wall(wall, tile_names);
     
+    // print opening message
     std::cout << "Welcome to mahjong!" << std::endl;
     std::cout << "The objective of the game is to create 4 sets of three sequences or triplets and one pair" << std::endl;
     std::cout << "Type q to quit anytime" << std::endl;
-    //std::cout << "Hand: " << std::endl;
+    
+    // generate wall and starting hand
+    shuffle_wall(wall);
+    depth = generate_hand(wall, hand);
+    
+    sort_hand(hand);
+    print_hand(hand, tile_names);
     std::cout << "Type the tile to discard" << std::endl;
     
     // game loop
