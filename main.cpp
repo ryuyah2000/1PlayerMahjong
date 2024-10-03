b689193c39fde6d90fad9b5499d7304d6f08b521
ryuyah2000 <ryuyah2000@users.noreply.github.com>
2024-10-02 21:16:09 -0700

    Added everything but the check win function
    Finished the game loop, game properly discards and draws tiles, options for ending the game after a win/loss,

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 
 #include <iostream>
+#include <sstream>
 #include <map>
 #include <array>
 #include <vector>
@@ -13,118 +14,257 @@
 
 // puts 4 copies of each tile into the wall
 void initialize_wall(std::array<int, 136> &wall, const std::map<int, std::string> tiles) {
-    int i = 0;
-    for (std::pair<int, std::string> t : tiles) {
-        // put 4 copies of each tile into the wall
-        wall.at(i++) = t.first;
-        wall.at(i++) = t.first;
-        wall.at(i++) = t.first;
-        wall.at(i++) = t.first;
-    }
+	int i = 0;
+	for (std::pair<int, std::string> t : tiles) {
+		// put 4 copies of each tile into the wall
+		wall.at(i++) = t.first;
+		wall.at(i++) = t.first;
+		wall.at(i++) = t.first;
+		wall.at(i++) = t.first;
+	}
 }
 
 
 // shuffles wall
 void shuffle_wall(std::array<int, 136> &wall) {
-    std::mt19937 rng(std::time(nullptr));
-    std::shuffle(wall.begin(), wall.end(), rng);
+	std::mt19937 rng(std::time(nullptr));
+	std::shuffle(wall.begin(), wall.end(), rng);
 }
 
 
 // generates hand from the first 14 tiles of the wall
 int generate_hand(std::array<int, 136> wall, std::vector<int> &hand) {
-    for (int i = 0; i < 14; ++i) {
-        hand.at(i) = wall.at(i);
-    }
-    return 14;
+	for (int i = 0; i < 14; ++i) {
+		hand.at(i) = wall.at(i);
+	}
+	return 14;
 }
 
 
+// prints out hand
+// kinda became complicated but basically just groups each tile with their suit
 void print_hand(std::vector<int> &hand, const std::map<int, std::string> tile_names) {
-    for (int i : hand) {
-        std::cout << tile_names.at(i);
-    }
-    std::cout << std::endl;
+	std::string man, pin, sou, honor = "";
+	for (int i = 0; i < hand.size() - 1; ++i) {
+		switch (hand.at(i) & 0xF0) {
+		case 0x10: // manzu
+			man += std::to_string(hand.at(i) & 0x0F);
+			break;
+		case 0x20: // pinzu
+			pin += std::to_string(hand.at(i) & 0x0F);
+			break;
+		case 0x30: // souzu
+			sou += std::to_string(hand.at(i) & 0x0F);
+			break;
+		case 0x40: // honor
+			honor += tile_names.at(hand.at(i));
+			break;
+		}
+	}
+	std::cout << man << "m " << pin << "p " << sou << "s " << honor << ", draw: " << tile_names.at(hand.at(hand.size() - 1));
+	std::cout << std::endl;
 }
 
+
+// sorts hand
 void sort_hand(std::vector<int> &hand) {
-    std::sort(hand.begin(), hand.end());
+	std::sort(hand.begin(), hand.end());
+}
+
+
+// check for win
+bool check_win(std::vector<int> &hand) {
+	return true;
 }
 
 
 int main() {
-    // map tile names to integer values for easier hand completeness calculation
-    const std::map<int, std::string> tile_names {
-        // manzu
-        { 0x11, "1m" },
-        { 0x12, "2m" },
-        { 0x13, "3m" },
-        { 0x14, "4m" },
-        { 0x15, "5m" },
-        { 0x16, "6m" },
-        { 0x17, "7m" },
-        { 0x18, "8m" },
-        { 0x19, "9m" },
-        
-        // pinzu
-        { 0x21, "1p" },
-        { 0x22, "2p" },
-        { 0x23, "3p" },
-        { 0x24, "4p" },
-        { 0x25, "5p" },
-        { 0x26, "6p" },
-        { 0x27, "7p" },
-        { 0x28, "8p" },
-        { 0x29, "9p" },
-        
-        // souzu
-        { 0x31, "1s" },
-        { 0x32, "2s" },
-        { 0x33, "3s" },
-        { 0x34, "4s" },
-        { 0x35, "5s" },
-        { 0x36, "6s" },
-        { 0x37, "7s" },
-        { 0x38, "8s" },
-        { 0x39, "9s" },
-        
-        // honor tiles
-        { 0x41, "E" },
-        { 0x42, "S" },
-        { 0x43, "W" },
-        { 0x44, "N" },
-        { 0x45, "Wh" },
-        { 0x46, "Gr" },
-        { 0x47, "Re" }
-    };
-    
-    
-    std::array<int, 136> wall; // 136 tiles total. 4 of each type of tile.
-    std::vector<int> hand(14); // 14 tiles in your hand
-    int depth; // how deep into the wall you are
-    initialize_wall(wall, tile_names);
-    
-    // print opening message
-    std::cout << "Welcome to mahjong!" << std::endl;
-    std::cout << "The objective of the game is to create 4 sets of three sequences or triplets and one pair" << std::endl;
-    std::cout << "Type q to quit anytime" << std::endl;
-    
-    // generate wall and starting hand
-    shuffle_wall(wall);
-    depth = generate_hand(wall, hand);
-    
-    sort_hand(hand);
-    print_hand(hand, tile_names);
-    std::cout << "Type the tile to discard" << std::endl;
-    
-    // game loop
-    std::string input;
-    while (input != "q") {
-        std::cin >> input;
-        if (input == "q") {
-            std::cout << "Thanks for playing!";
-        }
-    }
+	// map tile codes to their names as string literals
+	const std::map<int, std::string> tile_names {
+		// manzu
+		{ 0x11, "1m" },
+		{ 0x12, "2m" },
+		{ 0x13, "3m" },
+		{ 0x14, "4m" },
+		{ 0x15, "5m" },
+		{ 0x16, "6m" },
+		{ 0x17, "7m" },
+		{ 0x18, "8m" },
+		{ 0x19, "9m" },
 
-    return 0;
+		// pinzu
+		{ 0x21, "1p" },
+		{ 0x22, "2p" },
+		{ 0x23, "3p" },
+		{ 0x24, "4p" },
+		{ 0x25, "5p" },
+		{ 0x26, "6p" },
+		{ 0x27, "7p" },
+		{ 0x28, "8p" },
+		{ 0x29, "9p" },
+
+		// souzu
+		{ 0x31, "1s" },
+		{ 0x32, "2s" },
+		{ 0x33, "3s" },
+		{ 0x34, "4s" },
+		{ 0x35, "5s" },
+		{ 0x36, "6s" },
+		{ 0x37, "7s" },
+		{ 0x38, "8s" },
+		{ 0x39, "9s" },
+
+		// honor tiles
+		{ 0x41, "E" },
+		{ 0x42, "S" },
+		{ 0x43, "W" },
+		{ 0x44, "N" },
+		{ 0x45, "Wh" },
+		{ 0x46, "Gr" },
+		{ 0x47, "Re" }
+	};
+
+	// possible inputs for tiles
+	const std::map<std::string, int> eng_to_tilecodes {
+		// manzu
+		{ "1m", 0x11 },
+		{ "2m", 0x12 },
+		{ "3m", 0x13 },
+		{ "4m", 0x14 },
+		{ "5m", 0x15 },
+		{ "6m", 0x16 },
+		{ "7m", 0x17 },
+		{ "8m", 0x18 },
+		{ "9m", 0x19 },
+
+		// pinzu
+		{ "1p", 0x21 },
+		{ "2p", 0x22 },
+		{ "3p", 0x23 },
+		{ "4p", 0x24 },
+		{ "5p", 0x25 },
+		{ "6p", 0x26 },
+		{ "7p", 0x27 },
+		{ "8p", 0x28 },
+		{ "9p", 0x29 },
+
+		// souzu
+		{ "1s", 0x31 },
+		{ "2s", 0x32 },
+		{ "3s", 0x33 },
+		{ "4s", 0x34 },
+		{ "5s", 0x35 },
+		{ "6s", 0x36 },
+		{ "7s", 0x37 },
+		{ "8s", 0x38 },
+		{ "9s", 0x39 },
+
+		// honor tiles
+		{ "1z", 0x41 },
+		{ "e", 0x41 },
+		{ "east", 0x41 },
+
+		{ "2z", 0x42 },
+		{ "south", 0x42 },
+		{ "s", 0x42 },
+
+		{ "3z", 0x43 },
+		{ "w", 0x43 },
+		{ "west", 0x43 },
+
+		{ "4z", 0x44 },
+		{ "n", 0x44 },
+		{ "north", 0x44 },
+
+		{ "5z", 0x45 },
+		{ "wh", 0x45 },
+		{ "white", 0x45 },
+
+		{ "6z", 0x46 },
+		{ "gr", 0x46 },
+		{ "green", 0x46 },
+
+		{ "7z", 0x47 },
+		{ "re", 0x47 },
+		{ "red", 0x47 }
+	};
+
+
+	std::array<int, 136> wall; // 136 tiles total. 4 of each type of tile.
+	std::vector<int> hand(14); // 14 tiles in your hand
+	std::vector<int> discard(24); // discarded tiles, usually does not go above 24 from playtesting
+	int depth; // how deep into the wall you are
+	initialize_wall(wall, tile_names);
+
+	// print opening message
+	std::cout << "Welcome to mahjong!" << std::endl;
+	std::cout << "The objective of the game is to create 4 sets of three sequences or triplets and one pair" << std::endl;
+	std::cout << "Type \"mahjong\" to win" << std::endl;
+	std::cout << "But be careful! If you hand isn't valid, the round is over." << std::endl;
+	std::cout << "Type q to quit anytime" << std::endl;
+
+	// generate wall and starting hand
+	shuffle_wall(wall);
+	depth = generate_hand(wall, hand);
+	sort_hand(hand);
+
+	// game loop
+	std::string input;
+	while (input != "q") {
+		print_hand(hand, tile_names);
+		std::cout << "Type the tile to discard" << std::endl;
+		std::cout << "> ";
+
+		std::cin >> input;
+		auto tile = eng_to_tilecodes.find(input); // validate tile input
+		if (tile != eng_to_tilecodes.end() && depth < 136) {
+			// valid tile input, look for it in hand
+			auto tilecode = std::find(hand.begin(), hand.end(), tile->second);
+			if (tilecode != hand.end()) {
+				// found, replace the tile
+				hand.erase(tilecode);
+				sort_hand(hand);
+				hand.push_back(wall.at(depth++));
+			} else {
+				// print tile not found message
+				std::cout << "Tile not found in hand" << std::endl;
+			}
+		} else if (input == "mahjong" || depth >= 136) {
+			// check for win
+			if (check_win(hand)) {
+				// win, add score and reset game
+				std::cout << "Congratulations! You won!" << std::endl;
+				std::cout << "Play again? (Y/N)" << std::endl;
+				std::cin >> input;
+				if (input == "n" || input == "q") {
+					std::cout << "Thanks for playing!";
+					input = "q"; // quit the game loop
+				} else {
+					// reset game
+					shuffle_wall(wall);
+					depth = generate_hand(wall, hand);
+					sort_hand(hand);
+				}
+			} else {
+				// not a win, reset game
+				std::cout << "This is not a winning hand..." << std::endl;
+				std::cin >> input;
+				if (input == "n" || input == "q") {
+					std::cout << "Thanks for playing!";
+					input = "q"; // quit the game loop
+				} else {
+					// reset game
+					shuffle_wall(wall);
+					depth = generate_hand(wall, hand);
+					sort_hand(hand);
+				}
+			}
+		} else if (input == "q") {
+			std::cout << "Thanks for playing!";
+		} else {
+			std::cout << "Invalid tile" << std::endl;
+		}
+	}
+
+	return 0;
 }
