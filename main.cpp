5033b59accc5bdd6574706428b747ff122621f1e
ryuyah2000 <ryuyah2000@users.noreply.github.com>
2024-10-03 00:51:51 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 
 
 #include <iostream>
-#include <sstream>
 #include <map>
 #include <array>
 #include <vector>
@@ -72,9 +71,103 @@ void sort_hand(std::vector<int> &hand) {
 }
 
 
+// check win helper
+// assumes hand is sorted
+bool check_win(std::vector<int> &hand, std::vector<bool> &visited, std::vector<bool> &win, bool pair_found = false) {
+
+	// base case
+	if (std::find(win.begin(), win.end(), false) == win.end()) {
+		return true;
+	}
+
+	for (int i = 0; i < hand.size(); ++i) {
+		if (!visited.at(i)) {
+
+			// check subsequent tiles to see if they make a sequence, pair, or triplet
+			for (int j = i + 1; j < hand.size(); ++j) {
+				if (!visited.at(j)) {
+					// if not visited, check if it makes a pair with the original
+					if (hand.at(i) == hand.at(j)) {
+						// pair found, mark as visited and won and search for next shape
+						// if no pair found yet
+						if (!pair_found) {
+							visited.at(i) = visited.at(j) = true;
+							win.at(i) = win.at(j) = true;
+//std::cout<<"DEBUG: PAIR: "<<std::hex<<hand.at(i)<<hand.at(j)<<std::endl;
+							// check hand again for more shapes
+							if (check_win(hand, visited, win, true)) {
+								return true;
+							} else {
+								// unmark as visited and won
+								win.at(i) = win.at(j) = false;
+								visited.at(i) = visited.at(j) = false;
+							}
+
+						// otherwise, check for triplets
+						} else {
+							// check for triplet
+							if (j + 1 >= hand.size()) {
+								// invalid hand, return false
+								return false;
+							} else {
+								if (hand.at(j) == hand.at(j + 1)) {
+									// triplet found, mark as visited and won and search for next shape
+									visited.at(i) = visited.at(j) = visited.at(j + 1) = true;
+									win.at(i) = win.at(j) = win.at(j + 1) = true;
+//std::cout<<"DEBUG: TRIP: "<<std::hex<<hand.at(i)<<hand.at(j)<<hand.at(j+1)<<std::endl;
+									if (check_win(hand, visited, win, pair_found)) {
+										return true;
+									} else {
+										win.at(i) = win.at(j) = win.at(j + 1) = false;
+										visited.at(i) = visited.at(j) = visited.at(j + 1) = false;
+									}
+								}
+							}
+						}
+					}
+
+
+					// check for sequence
+					if (hand.at(i) + 1 == hand.at(j)) {
+						// start of a sequence, check for another
+						for (int k = j + 1; k < hand.size(); ++k) {
+							if (hand.at(k) > hand.at(j) + 1) {
+								break;
+							}
+							if (!visited.at(k)) {
+								if (hand.at(j) + 1 == hand.at(k)) {
+									// sequence found, mark as visited and won and search for next shape
+									visited.at(i) = visited.at(j) = visited.at(k) = true;
+									win.at(i) = win.at(j) = win.at(k) = true;
+//std::cout<<"DEBUG: SEQU: "<<std::hex<<hand.at(i)<<hand.at(j)<<hand.at(k)<<std::endl;
+									if (check_win(hand, visited, win, pair_found)) {
+										return true;
+									} else {
+										win.at(i) = win.at(j) = win.at(k) = false;
+										visited.at(i) = visited.at(j) = visited.at(k) = false;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return false;
+}
+
+
 // check for win
 bool check_win(std::vector<int> &hand) {
-	return true;
+	// create a checked tile array
+	std::vector<bool> win(hand.size(), false);
+	std::vector<bool> visited(hand.size(), false);
+	sort_hand(hand);
+
+	// run the actual checker
+	return check_win(hand, visited, win);
 }
 
 
